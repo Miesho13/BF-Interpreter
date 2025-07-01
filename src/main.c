d10135201afd0b8403c5a1f6d40042bedcc0c6f9
miesho <marcin.ryzewskii@gmail.com>
2025-07-01 22:46:29 +0200

    Refactor entry point of program.

--- src/main.c
+++ src/main.c
@@ -1,45 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
 #include <sys/stat.h>
-
-int is_file(const char *path) {
-    struct stat st;
-    return (stat(path, &st) == 0) && S_ISREG(st.st_mode);
-}
-
-long get_file_size(FILE* f) {
-    fseek(f, 0, SEEK_END);
-    long size = ftell(f);
-    fseek(f, 0, SEEK_SET);
-    return size;
-}
-
-uint8_t* open_file(const char* path) {
-    if (!is_file(path)) {
-        fprintf(stderr, "Error: Not a file.");
-        return NULL;
-    }
-
-    FILE* f = fopen(path, "r");
-    if (!f) {
-        fprintf(stderr, "Error: File can't be open.");
-        return NULL;
-    }
-
-    long size = get_file_size(f);
-    if (size <= 0) {
-        fprintf(stderr, "Error: file cant be 0 file");
-        return NULL;
-    }
-
-    uint8_t *bf_file = (uint8_t*)malloc(size + 1);
-    bf_file[size] = 0;
-    fread(bf_file, 1, size, f);
-    fclose(f);
-
-    return bf_file;
-}
+#include "out_asm.h"
+#include "bf_file.h"
+#include "stack.h"
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -47,10 +11,20 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    uint8_t* bf_content = open_file(argv[1]);
-    if (!bf_content) {
+    bf_file bf = open_file(argv[1]);
+    if (bf.len == 0) {
         return 2;
     }
     
+    asm_out asm_file = crate_asm(&bf);
+    printf("%s", asm_file.buffer);
+
+    if (save_out_asm(&asm_file) != 0) {
+        return 3;
+    }
+
+    if (call_fasm_cpm() != 0) {
+        return 3;
+    }
     return 0;
 }
